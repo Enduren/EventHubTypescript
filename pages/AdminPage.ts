c5a84c19438795718d76db4e36948b5ad1429251
dexter <dtennison79@gmail.com>
2026-07-17 16:02:52 -0500

    update page

--- pages/AdminPage.ts
+++ pages/AdminPage.ts
@@ -2,6 +2,9 @@ import { BasePage } from './BasePage';
 
 export class AdminPage extends BasePage {
     //define selectors
+    readonly dashboardHeader = this.locator('text=Admin Dashboard');
+
+
 
     
 
--- pages/MyBookings.ts
+++ pages/MyBookings.ts
@@ -1,6 +1,8 @@
 import { BasePage } from './BasePage';
 
 export class MyBookingsPage extends BasePage {
+    //define selectors
+    readonly dashboardHeader = this.locator('text=My Bookings');
 
 
 }
